c068084f4a60cfd24c154e18667c6430f18efe4d
BhaveshG2311 <bhaveshg608@gmail.com>
2026-07-03 01:13:20 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+import warnings
+warnings.filterwarnings("ignore")
+
+model = joblib.load('best_model.pkl')
+st.title("Student Exam Score Prediction")
+st.write("Enter the number of hours studied to predict the exam score.")
+
+study_hours = st.slider("Study Hours per day", 0.0, 12.0,2.0)
+attendance=st.slider("Attendance percentage", 0.0, 100.0, 80.0)
+mental_health=st.slider("Mental Health Score (1-10)", 1, 10, 5)
+sleep_hours=st.slider("Sleep Hours per night", 0.0, 12.0, 7.0)
+part_time_job=st.selectbox("Do you have a part-time job?", ["Yes", "No"])
+
+ptj_encoded=1 if part_time_job == "Yes" else 0
+if st.button("Predict Exam Score"):
+    input_data = np.array([[study_hours, attendance, mental_health, sleep_hours, ptj_encoded]])
+    prediction = model.predict(input_data)[0]
+    prediction = max(0, min(100, prediction))  
+    st.success(f"Predicted Exam Score: {prediction:.2f}")
